47b6c1af22f55309a683b3301dc4b63f4944899f
Ryan <redryma@icloud.com>
2024-09-17 16:22:50 -0400

    Stuff

--- src/main/java/com/ryorama/tstpcontent/events/CommonEvents.java
+++ src/main/java/com/ryorama/tstpcontent/events/CommonEvents.java
@@ -34,18 +34,7 @@ import java.util.List;
 
 @Mod.EventBusSubscriber(modid = TstpContentMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class CommonEvents {
-
-    @SubscribeEvent
-    public static void addItemsToCreativeTabs(BuildCreativeModeTabContentsEvent event) {
-        if (event.getTabKey().equals(CreativeModeTabs.FOOD_AND_DRINKS)) {
-            event.accept(CannibalsCallingModItems.HUMAN_MEAT.get());
-            event.accept(CannibalsCallingModItems.COOKED_HUMAN_MEAT.get());
-            event.accept(CannibalsCallingModItems.INHUMAN_MEAT.get());
-        }
-    }
-
-
-    //ToDo: Fix
+    //ToDo: Sort out Kotlin implementation
     /*
     @SubscribeEvent
     public static void tickEvent(TickEvent.LevelTickEvent event) {
